e6e0d150073b10a139f92f44da5ace49c4f3beaf
Vasili_Balyka <Vasili_Balyka@epam.com>
2023-03-29 19:43:06 +0400

    Feat | Spring Security | added custom login and logout forms

--- security-app/src/main/java/com/epam/edu/config/WebSecurityConfig.java
+++ security-app/src/main/java/com/epam/edu/config/WebSecurityConfig.java
@@ -1,6 +1,13 @@
 package com.epam.edu.config;
 
 import com.epam.edu.model.Authority;
+import com.epam.edu.storage.AuthorityRepository;
+import com.epam.edu.storage.UserRepository;
+import com.epam.edu.storage.model.AuthorityEntity;
+import com.epam.edu.storage.model.UserEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
@@ -12,33 +19,87 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
+@Slf4j
 public class WebSecurityConfig {
+
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private AuthorityRepository authorityRepository;
 
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
       UserDetailsRepositoryReactiveAuthenticationManager authenticationManager) {
     return http.authorizeExchange()
+        .pathMatchers("/login").permitAll()
+        .pathMatchers("/lout").permitAll()
         .pathMatchers("/actuator/health").permitAll()
         .pathMatchers("/actuator/info").hasAuthority(Authority.INFO.getAuthority())
         .pathMatchers("/actuator/env").hasAuthority(Authority.ADMIN.getAuthority())
         .anyExchange().authenticated()
         .and()
         .authenticationManager(authenticationManager)
-        .formLogin()
+        .formLogin().loginPage("/login")
         .and()
-        .logout()
+        .logout().logoutUrl("/logout")
         .and()
         .exceptionHandling()
         .and()
+        .csrf().disable()
         .build();
   }
 
   @Bean
-  public UserDetailsRepositoryReactiveAuthenticationManager authenticationManager(ReactiveUserDetailsService userDetailsService) {
+  public UserDetailsRepositoryReactiveAuthenticationManager authenticationManager(
+      ReactiveUserDetailsService userDetailsService) {
     var authManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
     authManager.setPasswordEncoder(new BCryptPasswordEncoder(10));
     return authManager;
+  }
 
+  @Bean
+  @ConditionalOnProperty(name = "database.example", havingValue = "true")
+  public void exampleEntities() {
+    setAuthorityRepository();
+    setUserRepository();
+  }
+
+  private void setAuthorityRepository() {
+    log.info("Setting authority");
+    if (authorityRepository.count() == 0) {
+      authorityRepository.save(
+          AuthorityEntity.builder()
+              .authority(Authority.INFO)
+              .build());
+      authorityRepository.save(
+          AuthorityEntity.builder()
+              .authority(Authority.ADMIN)
+              .build());
+      log.info("Authority set");
+    }
+  }
+
+  private void setUserRepository() {
+    log.info("Setting users");
+    if (userRepository.count() == 0) {
+      var encoder = new BCryptPasswordEncoder(10);
+      var infoUser = UserEntity.builder()
+          .login("info")
+          .password(encoder.encode("info"))
+          .build();
+      var infoAuth = authorityRepository.findByAuthority(Authority.INFO);
+      infoUser.addAuthority(infoAuth);
+      userRepository.save(infoUser);
+      var adminUser = UserEntity.builder()
+          .login("admin")
+          .password(encoder.encode("admin"))
+          .build();
+      var adminAuth = authorityRepository.findByAuthority(Authority.ADMIN);
+      adminUser.addAuthority(adminAuth);
+      adminUser.addAuthority(infoAuth);
+      userRepository.save(adminUser);
+      log.info("Users set");
+    }
   }
 
 }
